fc538aa5049b571b296b850698a03a738e68a4c1
asilvas <asilvas@godaddy.com>
2013-09-13 15:48:30 -0700

    7 new test cases for restart cmds

---
+++ test/start-restart.js
@@ -0,0 +1,69 @@
+var cservice = require("../cluster-service");
+var assert = require("assert");
+
+describe('Restart', function(){
+	describe('Start workers', function(){
+		it('1 worker should be running', function(done){
+			cservice.start("./test/workers/basic",  { workerCount: 2, accessKey: "123", cliEnabled: false }, function() {
+				assert.equal(cservice.workers.length, 2, "2 workers expected, but " + cservice.workers.length + " found");
+				done();
+			});
+		});  
+	});
+
+	describe('Bad input #1', function(){  
+		it('Bad input command', function(done){
+			cservice.trigger("restart", function(err) {
+				assert.equal(err, "Invalid request. Try help restart");
+				done();
+			}, "GG"
+			);
+		});
+	});
+
+	describe('Bad input #2', function(){  
+		it('Bad input type', function(done){
+			cservice.trigger("restart", function(err) {
+				assert.equal(err, "Invalid request. Try help restart");
+				done();
+			}, 0
+			);
+		});
+	});
+
+	describe('Restart workers', function(){  
+		it('2 workers should be running', function(done){
+			cservice.trigger("restart", function() {
+				assert.equal(cservice.workers.length, 2, "2 workers expected, but " + cservice.workers.length + " found");
+				done();
+			}, "all", 30000 /* with timeout */
+			);
+		});  
+		it('timeout', function(done){
+			cservice.trigger("restart", function(err) {
+				assert.equal(err, "timed out");
+				done();
+			}, "all", 100 /* with timeout */
+			);
+		});
+	});
+	
+	describe('Stop workers', function(){  
+		it('0 workers should be running', function(done){
+			cservice.stop(30000, function() {
+				assert.equal(cservice.workers.length, 0, "0 workers expected, but " + cservice.workers.length + " found");
+				done();
+			});
+		});  
+	});
+	
+	describe('Restart with no workers', function(){  
+		it('Expect restart failure', function(done){
+			cservice.trigger("restart", function(err) {
+				assert.equal(err, "No workers to restart");
+				done();
+			}, "all"
+			);
+		});
+	});
+})
