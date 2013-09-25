4ccdd63d522002cf121df314b246c8c7e40b5c23
asilvas <asilvas@godaddy.com>
2013-09-25 13:33:38 -0700

    up coverage

--- test/server-rest.js
+++ test/server-rest.js
@@ -7,62 +7,75 @@ var request = require("request");
 cservice.options.log = function() {};
 cservice.isWorker && it("WORKER", function(done) { });
 cservice.isMaster && describe('REST Server', function(){
-	describe('Start worker', function(){
-		it('0 workers should be running', function(done){
-			cservice.start(null,  { workerCount: 0, accessKey: "123", cliEnabled: false }, function() {
-				assert.equal(cservice.workers.length, 0, "0 worker expected, but " + cservice.workers.length + " found");
-				done();
-			});
-		});  
-	});
-	
-	describe('Health check', function(){
-		httpclient.init(cservice.locals, extend(cservice.options, { accessKey: "123", silentMode: true }));
-		it('OK should be returned', function(done){
-			httpclient.execute("health", function(err, result) {
-				assert.equal(result, "'OK'", "Expected OK. result=" + result + ", err=" + err);
-				done();
-			}
-			);
+	it('Start worker', function(done){
+		cservice.start(null,  { workerCount: 0, accessKey: "123", cliEnabled: false }, function() {
+			assert.equal(cservice.workers.length, 0, "0 worker expected, but " + cservice.workers.length + " found");
+			done();
 		});
-	});
-
-	describe('Bad command', function(){
-		it('Should fail', function(done){
-			httpclient.execute("x98s7df987sdf", function(err, result) {
-				assert.equal(result, "Not found. Try /help", "Expected 'Not found. Try /help'. result=" + result + ", err=" + err);
-				done();
-			}
-			);
-		});  
-	});
-
-	describe('Method Not Allowed', function(){
-		it('405', function(done){
-			var url = "http://localhost:11987/cli?cmd=health&accessKey=123";
-			request.get(url, function (err, res, result) {
-				assert.equal(result, "Method Not Allowed", "Expected 'Method Not Allowed'. result=" + result + ", err=" + err);
-				done();
-			});		
-		});  
-	});
-
-	describe('Page Not Found', function(){
-		it('404', function(done){
-			var url = "http://localhost:11987/BADCLI?cmd=health&accessKey=123";
-			request.post(url, function (err, res, result) {
-				assert.equal(result, "Page Not Found", "Expected 'Page Not Found'. result=" + result + ", err=" + err);
-				done();
-			});		
-		});  
-	});
+	});  
 	
-	describe('Stop workers', function(){  
-		it('0 workers should be running', function(done){
-			cservice.stop(30000, function() {
-				assert.equal(cservice.workers.length, 0, "0 workers expected, but " + cservice.workers.length + " found");
-				done();
-			});
-		});  
+	httpclient.init(cservice.locals, extend(cservice.options, { accessKey: "123", silentMode: true }));
+	it('Health check', function(done){
+		httpclient.execute("health", function(err, result) {
+			assert.equal(result, "'OK'", "Expected OK. result=" + result + ", err=" + err);
+			done();
+		}
+		);
+	});
+
+	it('Bad command', function(done){
+		httpclient.execute("x98s7df987sdf", function(err, result) {
+			assert.equal(result, "Not found. Try /help", "Expected 'Not found. Try /help'. result=" + result + ", err=" + err);
+			done();
+		}
+		);
+	});  
+
+	var disabledCmd = function (evt, cb) {
+		cb(null, "You shouldn't be able to see my data");
+	};
+	disabledCmd.control = function() {
+		return "disabled";
+	};
+
+	it('Command authorization', function(done){
+		cservice.on("disabledCmd", disabledCmd);
+		var url = "http://localhost:11987/cli?cmd=disabledCmd&accessKey=123";
+		request.post(url, function (err, res, result) {
+			assert.equal(result, "Not authorized to execute 'disabledCmd' remotely");
+			done();
+		});		
+	});
+
+	it('Request authorization', function(done){
+		cservice.on("disabledCmd", disabledCmd);
+		var url = "http://localhost:11987/cli?cmd=disabledCmd&accessKey=BAD";
+		request.post(url, function (err, res, result) {
+			assert.equal(result, "Not authorized");
+			done();
+		});		
+	});
+
+	it('Method Not Allowed', function(done){
+		var url = "http://localhost:11987/cli?cmd=health&accessKey=123";
+		request.get(url, function (err, res, result) {
+			assert.equal(result, "Method Not Allowed", "Expected 'Method Not Allowed'. result=" + result + ", err=" + err);
+			done();
+		});		
+	});  
+
+	it('Page Not Found', function(done){
+		var url = "http://localhost:11987/BADCLI?cmd=health&accessKey=123";
+		request.post(url, function (err, res, result) {
+			assert.equal(result, "Page Not Found", "Expected 'Page Not Found'. result=" + result + ", err=" + err);
+			done();
+		});		
+	});  
+	
+	it('Stop workers', function(done){
+		cservice.stop(30000, function() {
+			assert.equal(cservice.workers.length, 0, "0 workers expected, but " + cservice.workers.length + " found");
+			done();
+		});
 	});
 })
